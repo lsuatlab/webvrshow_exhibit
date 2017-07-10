6b54461cefdaaa7ca80f177650e61d635e4fec22
Derick Ostrenko <dostrenko@gmail.com>
2017-07-10 14:37:07 -0500

    added _site

---
+++ _site/assets/js/slideshow.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  $('#imageGallery').lightSlider({
+      gallery:true,
+      item:1,
+      loop:true,
+      enableDrag:true,
+      keyPress: true,
+      // thumbItem:10,
+      slideMargin:0
+      // currentPagerPosition:'left',
+  });
+});
+
+var myPlayer = videojs('work-video', { fluid: true });
+
+
+// $('#myvideo').hover(function toggleControls() {
+//     if (this.hasAttribute("controls")) {
+//         this.removeAttribute("controls")
+//     } else {
+//         this.setAttribute("controls", "controls")
+//     }
+// })
+
+// var video = document.getElementById('video');
+// video.addEventListener('click',function(){
+//     video.play();
+// },false);
+
+
+
+
+// $(document).ready(function() {
+//   $('#imageGallery').lightSlider({
+//       // gallery:true,
+//       item:1,
+//       // loop:true,
+//       // thumbItem:10,
+//       slideMargin:0,
+//       // currentPagerPosition:'left',
+//       onSliderLoad: function(el) {
+//           el.lightGallery({
+//               selector: '#imageGallery .lslide',
+//               zoon: false
+//                // videojs: true
+//           });
+//       }   
+//   });
+
+
+
+// });
+
+// $('#imageGallery').lightGallery({
+//     videojs: true
+// }); 
